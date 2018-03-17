7ab1e62d0de7a864a8c63ef4cf20464b2e1aab3e
Tony F <ohmygod525@gmail.com>
2018-03-16 23:04:16 -0700

    fix app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Main file.");
+const http = require("http");
+
+var port=8080;
+var ip="127.0.0.1";
+
+http.createServer(function(req, res)
+	{
+		res.writeHead(200, {"Content-Type":"text/plain"});
+		res.write("Hello");
+		res.end();
+	}
+).listen(port, ip);
+
