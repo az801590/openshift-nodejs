0912b69dfd46577977fc17a2235eab05dbc71e03
Tony F <ohmygod525@gmail.com>
2018-03-17 00:24:31 -0700

    fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 var port=process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip=process.env.IP || process.nv.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var ip=process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 http.createServer(function(req, res)
 	{
